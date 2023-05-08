620cce3f52be2123943c5a167a1b8fb6151ff35e
Eli Speigel <eli.speigel@gmail.com>
2023-05-08 12:04:54 -0700

    refactor: Update main.go to use a struct to parse command line flags and reorganize functions

--- cmd/spocker/main.go
+++ cmd/spocker/main.go
@@ -6,7 +6,6 @@ import (
 	"net"
 	"os"
 	"os/exec"
-	"strconv"
 	"syscall"
 
 	"spocker/internal/container"
@@ -17,6 +16,20 @@ import (
 	"go.uber.org/zap"
 )
 
+type Config struct {
+	MemoryLimit    int
+	CPUShares      int
+	BlkioWeight    int
+	CgroupName     string
+	NamespaceName  string
+	NamespaceType  namespace.NamespaceType
+	FSRoot         string
+	NetworkName    string
+	NetworkIPCIDR  string
+	NetworkGateway string
+}
+
+// usage prints the command usage information.
 func usage() {
 	fmt.Fprintf(os.Stderr, "Usage: %s COMMAND\n", os.Args[0])
 	flag.PrintDefaults()
@@ -26,11 +39,34 @@ func main() {
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 
+	config, err := parseFlags()
+	if err != nil {
+		logger.Error("Error parsing flags", zap.Error(err))
+		usage()
+		os.Exit(1)
+	}
+
+	if len(flag.Args()) == 0 {
+		usage()
+		os.Exit(1)
+	}
+
+	switch flag.Args()[0] {
+	case "run":
+		runContainer(config, logger)
+	default:
+		usage()
+		os.Exit(1)
+	}
+}
+
+// parseFlags parses command line flags and returns a Config struct.
+func parseFlags() (*Config, error) {
 	flag.Usage = usage
 
-	memoryLimitFlag := flag.String("memory-limit", "", "Memory limit for the container in bytes")
-	cpuSharesFlag := flag.String("cpu-shares", "", "CPU shares for the container")
-	blkioWeightFlag := flag.String("blkio-weight", "", "Block I/O weight for the container")
+	memoryLimitFlag := flag.Int("memory-limit", 0, "Memory limit for the container in bytes")
+	cpuSharesFlag := flag.Int("cpu-shares", 0, "CPU shares for the container")
+	blkioWeightFlag := flag.Int("blkio-weight", 0, "Block I/O weight for the container")
 	cgroupNameFlag := flag.String("cgroup-name", "", "cgroup name for the container")
 	namespaceNameFlag := flag.String("namespace-name", "", "namespace name for the container")
 	namespaceTypeFlag := flag.Int("namespace-type", 0, "namespace type for the container")
@@ -41,75 +77,52 @@ func main() {
 
 	flag.Parse()
 
-	if len(flag.Args()) == 0 {
-		usage()
-		os.Exit(1)
-	}
-
-	switch flag.Args()[0] {
-	case "run":
-		memoryLimit, err := strconv.Atoi(*memoryLimitFlag)
-		if err != nil {
-			logger.Error("spocker: invalid memory limit", zap.String("memory limit", *memoryLimitFlag))
-			os.Exit(1)
-		}
-		cpuShares, err := strconv.Atoi(*cpuSharesFlag)
-		if err != nil {
-			logger.Error("spocker: invalid CPU shares", zap.String("CPU shares", *cpuSharesFlag))
-			os.Exit(1)
-		}
-		blkioWeight, err := strconv.Atoi(*blkioWeightFlag)
-		if err != nil {
-			logger.Error("spocker: invalid block I/O weight", zap.String("block I/O weight", *blkioWeightFlag))
-			os.Exit(1)
-		}
-
-		cgroupSpec := &cgroup.Spec{
-			Name: *cgroupNameFlag,
-			Resources: &cgroup.Resources{
-				Memory: &cgroup.Memory{
-					Limit: memoryLimit,
-				},
-				CPU: &cgroup.CPU{
-					Shares: cpuShares,
-				},
-				BlkIO: &cgroup.BlkIO{
-					Weight: blkioWeight,
-				},
-			},
-		}
-
-		namespaceSpec := &namespace.NamespaceSpec{
-			Name: *namespaceNameFlag,
-			Type: namespace.NamespaceType(*namespaceTypeFlag),
-		}
-
-		_, ipNet, err := net.ParseCIDR(*networkIPCIDRFlag)
-		if err != nil {
-			logger.Error("Invalid CIDR", zap.String("CIDR", *networkIPCIDRFlag), zap.Error(err))
-			return
-		}
-
-		networkConfig := &network.Config{
-			Name:    *networkNameFlag,
-			IPNet:   ipNet,
-			Gateway: net.ParseIP(*networkGatewayFlag),
-		}
-
-		run(cgroupSpec, namespaceSpec, *fsRootFlag, networkConfig)
-	default:
-		usage()
-		os.Exit(1)
-	}
+	return &Config{
+		MemoryLimit:    *memoryLimitFlag,
+		CPUShares:      *cpuSharesFlag,
+		BlkioWeight:    *blkioWeightFlag,
+		CgroupName:     *cgroupNameFlag,
+		NamespaceName:  *namespaceNameFlag,
+		NamespaceType:  namespace.NamespaceType(*namespaceTypeFlag),
+		FSRoot:         *fsRootFlag,
+		NetworkName:    *networkNameFlag,
+		NetworkIPCIDR:  *networkIPCIDRFlag,
+		NetworkGateway: *networkGatewayFlag,
+	}, nil
 }
 
-func run(cgroupSpec *cgroup.Spec, namespaceSpec *namespace.NamespaceSpec, fsRoot string, networkConfig *network.Config) {
-	logger, _ := zap.NewProduction()
-	defer logger.Sync()
+// runContainer runs a container using the provided configuration and logger.
+func runContainer(config *Config, logger *zap.Logger) {
+	cgroupSpec := &cgroup.Spec{
+		Name: config.CgroupName,
+		Resources: &cgroup.Resources{
+			Memory: &cgroup.Memory{
+				Limit: config.MemoryLimit,
+			},
+			CPU: &cgroup.CPU{
+				Shares: config.CPUShares,
+			},
+			BlkIO: &cgroup.BlkIO{
+				Weight: config.BlkioWeight,
+			},
+		},
+	}
 
-	if os.Geteuid() != 0 {
-		logger.Error("spocker: need root privileges")
-		os.Exit(1)
+	namespaceSpec := &namespace.NamespaceSpec{
+		Name: config.NamespaceName,
+		Type: config.NamespaceType,
+	}
+
+	_, ipNet, err := net.ParseCIDR(config.NetworkIPCIDR)
+	if err != nil {
+		logger.Error("Invalid CIDR", zap.String("CIDR", config.NetworkIPCIDR), zap.Error(err))
+		return
+	}
+
+	networkConfig := &network.Config{
+		Name:    config.NetworkName,
+		IPNet:   ipNet,
+		Gateway: net.ParseIP(config.NetworkGateway),
 	}
 
 	cmd := exec.Command(flag.Args()[1], flag.Args()[2:]...)
@@ -120,8 +133,15 @@ func run(cgroupSpec *cgroup.Spec, namespaceSpec *namespace.NamespaceSpec, fsRoot
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	if err := container.Run(cmd, cgroupSpec, namespaceSpec, fsRoot, networkConfig); err != nil {
-		logger.Error("spocker: error running container", zap.Error(err))
-		os.Exit(1)
+	err = container.Run(
+		cmd,
+		cgroupSpec,
+		namespaceSpec,
+		config.FSRoot,
+		networkConfig,
+	)
+	if err != nil {
+		logger.Error("Failed to run container", zap.Error(err))
+		return
 	}
 }
